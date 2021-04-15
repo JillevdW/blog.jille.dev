11364509cb9ddff765b907c1f7ccfe94de739795
JillevdW <Jillevdweerd@iCloud.com>
2021-04-15 18:43:08 +0200

    tags styling

--- components/tags.tsx
+++ components/tags.tsx
@@ -7,7 +7,7 @@ const Tags = ({ tags, className }: Props) => {
   return (
     <div className={`flex justify-start items-center ${className}`}>
       {tags.map(tag => (
-        <span className="bg-red-400 rounded py-1 px-3 mr-3 font-bold" key={tag}>{tag}</span>
+        <span className="text-gray-100 dark:text-white bg-red-400 rounded py-1 px-3 mr-3 font-bold" key={tag}>{tag}</span>
       ))}
     </div>
   )
