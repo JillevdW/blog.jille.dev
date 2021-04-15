fe02780be30b672264f61fee8a6d5bfa95f686e1
JillevdW <Jillevdweerd@iCloud.com>
2021-04-15 18:47:19 +0200

    adds page for tags

--- components/related-posts.tsx
+++ components/related-posts.tsx
@@ -2,15 +2,20 @@ import PostPreview from './post-preview'
 import Post from '../types/post'
 
 type Props = {
-  posts: Post[]
+  posts: Post[],
+  showsTitle?: Boolean
 }
 
-const RelatedPosts = ({ posts }: Props) => {
+const RelatedPosts = ({ posts, showsTitle = true }: Props) => {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        Related Posts
-      </h2>
+      { showsTitle 
+        ? <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+            Related Posts
+          </h2>
+        : ''
+      }
+      
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
           <PostPreview
---
+++ pages/tags/[tag].tsx
@@ -0,0 +1,101 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import Container from '../../components/container'
+import PostBody from '../../components/post-body'
+import Header from '../../components/header'
+import PostHeader from '../../components/post-header'
+import Layout from '../../components/layout'
+import { getRelatedPosts } from '../../lib/api'
+import PostTitle from '../../components/post-title'
+import Head from 'next/head'
+import { CMS_NAME } from '../../lib/constants'
+import PostType from '../../types/post'
+import RelatedPosts from '../../components/related-posts'
+
+type Props = {
+  posts: PostType[],
+  tag: string
+}
+
+const Tag = ({ posts, tag }: Props) => {
+  const router = useRouter()
+  if (!router.isFallback && !tag) {
+    return <ErrorPage statusCode={404} />
+  }
+  return (
+    <Layout>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article>
+              <Head>
+                <title>
+                  {tag} | Next.js Blog Example with {CMS_NAME}
+                </title>
+              </Head>
+              <section className="flex items-center justify-start mt-8 mb-12">
+                <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight">
+                  More about <span className='text-red-400'>{tag}</span>.
+                </h1>
+              </section>
+            </article>
+
+            { posts.length > 0
+              ? <RelatedPosts posts={posts} showsTitle={false}/>
+              : ''
+            }
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+export default Tag
+
+type Params = {
+  params: {
+    tag: string
+  }
+}
+
+export async function getStaticProps({ params }: Params) {
+  const posts = getRelatedPosts('', [params.tag], [
+    'title',
+    'date',
+    'tags',
+    'slug',
+    'author',
+    'content',
+    'ogImage',
+    'coverImage',
+  ])
+
+  return {
+    props: {
+      posts,
+      tag: params.tag
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const allTags = [
+    'swift',
+    'combine'
+  ];
+
+  return {
+    paths: allTags.map((tag) => {
+      return {
+        params: {
+          tag,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}
